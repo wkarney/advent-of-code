8a44c12f0cc734bcbda0e444df57250c2d2d7421
Will Karnasiewicz <will.karnasiewicz@boulevardcg.com>
2020-12-01 09:43:51 -0500

    generalized solution using itertools

--- 2020/1/will-202001.py
+++ 2020/1/will-202001.py
@@ -5,34 +5,23 @@ Advent of Code 2020, day 1
 Will Karnasiewicz
 """
 
-# Part 1
+import itertools
+import math
+
+# Input Data
 
 with open('./inputs/will-input.txt') as f:
     input_list = [int(line.rstrip('\n')) for line in f]
 
-def expense_report_two_to_total(expense_txt, total):
+# Parts 1 and 2
 
-    sorted_report = sorted(expense_txt)
-    for expense in sorted_report:
-        val_needed = total - expense
-        if val_needed in sorted_report:
-            return expense * val_needed
+def expense_report_pairs(expense_txt, total, quantity):
+    for group in list(itertools.combinations(expense_txt, quantity)):
+        if sum(group) == total:
+            return math.prod(group)
+    raise Exception("No solution found!")
 
-# Part 2
-
-def expense_report_three_to_total(expense_txt, total):
-
-    sorted_report = sorted(expense_txt)
-    for expense in sorted_report:
-        val_needed = total - expense
-        try:
-            subtotal = expense_report_two_to_total(expense_txt, val_needed)
-            return expense * subtotal
-        except:
-            continue
 
 if __name__ == "__main__":
-    print(expense_report_two_to_total(input_list, 2020))
-    print()
-    print(expense_report_three_to_total(input_list, 2020))
-
+    print(f"Part 1: {expense_report_pairs(input_list, 2020, 2)}")
+    print(f"Part 2: {expense_report_pairs(input_list, 2020, 3)}")
