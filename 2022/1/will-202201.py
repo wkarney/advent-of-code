31a9480cc90a8e779c7bf8cab0433e783b848ea7
Will Karnasiewicz <willkarnasiewicz@gmail.com>
2022-12-03 21:03:02 -0500

    2022 day 1

---
+++ 2022/1/will-202201.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+"""
+Advent of Code 2022, day 1
+Will Karnasiewicz
+"""
+
+import itertools
+import math
+
+# Input Data
+
+with open('./inputs/will-input.txt') as f:
+    elves = f.read().split('\n\n')
+    strings = [item.split('\n') for item in elves]
+    input_list = [list(map(int, item)) for item in strings]
+
+# # Parts 1 and 2
+
+def max_rations(inputs):
+    max_cals = 0
+    max_cals = sum(input_list[0])
+    for elf in input_list[1:]:
+        if sum(elf) >= max_cals:
+            max_cals = sum(elf)
+    return max_cals
+
+def top_rations(inputs):
+    max_cals = sorted([sum(elf) for elf in input_list[:3]])
+    for elf in input_list[3:]:
+        if sum(elf) >= max_cals[0]:
+            max_cals[0] = sum(elf)
+            max_cals = sorted(max_cals)
+    return sum(max_cals)
+
+
+if __name__ == "__main__":
+    print(f"Part 1: {max_rations(input_list)}")
+    print(f"Part 2: {top_rations(input_list)}")
